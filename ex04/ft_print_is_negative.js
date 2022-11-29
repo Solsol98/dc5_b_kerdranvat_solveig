a2f30e6b873aaded3bac14ab29ef4ef0fef7ba60
solveig98 <solveig.sol.kerdranvat@gmail.com>
2022-11-29 16:31:01 +0100

    ex04 - ok

---
+++ ex04/ft_print_is_negative.js
@@ -0,0 +1,11 @@
+function norp(entier) {
+    let result;
+    if (entier >= 0) {
+        return "P";
+    } else if (entier < 0) {
+        return "N";
+    } else {
+        return "Rentrez un nombre"
+    };
+    return console.log(numbers);
+}
